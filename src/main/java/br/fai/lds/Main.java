54653ed4eea5abcf80d8dadfde972fd1b875b19e
Anilson22 <pcassioferreira@gmail.com>
2022-03-21 00:32:25 -0300

    Primeiro Commit: criado main e cnfigurado maven

---
+++ src/main/java/br/fai/lds/Main.java
@@ -0,0 +1,15 @@
+package br.fai.lds;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main App = new Main();
+		App.start();
+		
+	}
+	
+	private static void start() {
+		
+	}
+
+}
