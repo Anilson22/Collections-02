b10ce4c7863a66389e09be06c6978acf2431559f
Anilson22 <pcassioferreira@gmail.com>
2022-03-21 00:42:46 -0300

    Feat: finazado map implementado try catch e alterando chave mg

--- src/main/java/br/fai/lds/Main.java
+++ src/main/java/br/fai/lds/Main.java
@@ -3,6 +3,8 @@ package br.fai.lds;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -26,6 +28,48 @@ public class Main {
             String valor = entry.getValue();
             System.out.println(String.format("chave: %s | valor: %s", chave, valor));
         }
+		
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+            String chave = entry.getKey();
+            String valor = entry.getValue();
+            if (chave.equals("ZORO")) {
+				System.out.println("\nChave: "+ chave + " Valor: " +valor);
+			}
+           
+        }
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+            String chave = entry.getKey();
+            String valor = entry.getValue();
+            if (chave.equals("MG")) {
+				System.out.println("\nChave: "+ chave + " Valor: " +valor);
+			}else {
+				System.out.println("Chave MG não existe");
+			}
+            
+        }
+		
+		 try {
+         	map.put("SRS", "Santa Rita");
+         	System.out.println("\nChave SRS Alterada Com Sucesso");
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Erro ao alterar valor da chave");
+			}
+        finally {
+			System.out.println("");
+		}
+		
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+            String chave = entry.getKey();
+            String valor = entry.getValue();
+            System.out.println(String.format("chave: %s | valor: %s", chave, valor));
+		
+		
+		
+		
+		
+		
+	}
+		System.out.println("Fim !!!");
 	}
 
 }
