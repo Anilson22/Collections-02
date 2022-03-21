10885ba35ed58b5008fc68d3f69c9bbb5bb1521c
Anilson22 <pcassioferreira@gmail.com>
2022-03-21 00:35:52 -0300

    Feat: adicionado map

--- src/main/java/br/fai/lds/Main.java
+++ src/main/java/br/fai/lds/Main.java
@@ -1,5 +1,8 @@
 package br.fai.lds;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,7 +12,20 @@ public class Main {
 	}
 	
 	private static void start() {
+		Map<String, String> map;
+		map = new HashMap<String, String>();
 		
+		map.put("SRS", "Santa Rita do Sapucai");
+		map.put("PA", "Pouso Alegre");
+		map.put("ITA", "Itajuba");
+		map.put("ZORO", "Conceição dos Ouros");
+		map.put("CAXU", "Cachoeira de Minas");
+		
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+            String chave = entry.getKey();
+            String valor = entry.getValue();
+            System.out.println(String.format("chave: %s | valor: %s", chave, valor));
+        }
 	}
 
 }
